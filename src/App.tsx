19621af7a23afe49c88caf4934fa5c6c08987e0c
Saga6569 <aristov6569@gmail.com>
2022-01-28 18:11:08 +0500

    coment

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,55 @@
 import React, { useState, useEffect} from 'react';
 
-const arrEN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const arrEN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //  массив из которого берутся букенные кардинаты
 
 const Table: React.FC<any> = (props) => {
   
-  const {height, width} = props.option;
+  const {height, width} = props.option;  // извлекаю из пропса  дынные
    
-  const creatingCoordinatesDigits = (a: number) => {
-    const result: Array<number> = []
+  const creatingCoordinatesDigits = (a: number) => { /// создает числовой массив длинной получаемого числа  
+    const result: Array<number> = [];
     for (let i = 1; i <= a; i++) {
-      result[i - 1] = i
+      result[i - 1] = i;
     }
-    return result
-  }
+    return result;
+  };
 
-  const initialLetterCoordinates: Array<string> = arrEN.slice(0, width);
-  const initialDigitCoordinates: Array<number> = creatingCoordinatesDigits(height);
+  const initialLetterCoordinates: Array<string> = arrEN.slice(0, width);  // создаем  массви кубвенных кардинат (пример width = 3 [A, B, C])
+  const initialDigitCoordinates: Array<number> = creatingCoordinatesDigits(height); // создаем  массви числовых кординат (пример height = 3 [1, 2, 3])
   
-  const [letterCoordinates, setLetterCoordinates] = useState(initialLetterCoordinates);
-  const [digitCoordinates, setDigitCoordinates] = useState(initialDigitCoordinates);
+  const [letterCoordinates, setLetterCoordinates] = useState(initialLetterCoordinates); // задаем начальное значение хукам 
+  const [digitCoordinates, setDigitCoordinates] = useState(initialDigitCoordinates); // задаем начальное значение хукам 
 
-  const createTable  = digitCoordinates.map((number) => {
-    const lineNumber = number
+  const createTable  = digitCoordinates.map((number) => {   //  создается  нумерованная колеция объектов с указание номером итерации и буквой 
+    const lineNumber = number;
       const cells = letterCoordinates.map((tab) => {
-        const iLetter = tab
+        const iLetter = tab;
         const keyNumber = number;
         const text = '';
         const state = 'open';
-        const cell = {iLetter, keyNumber, text, state}
+        const cell = {iLetter, keyNumber, text, state};
         return cell;
       })
-    return {lineNumber, cells}
+    return {lineNumber, cells};
   });
 
-  const [table, setTable] = useState(createTable);
+  const [table, setTable] = useState(createTable); // устанавливаем начальео  значение
 
-  useEffect((): void => {
-    setLetterCoordinates(initialLetterCoordinates)
-    setDigitCoordinates(initialDigitCoordinates)
+  useEffect((): void => { // эфект  срабатывается  если даные пропса меняются 
+    setLetterCoordinates(initialLetterCoordinates);
+    setDigitCoordinates(initialDigitCoordinates);
   }, [props]);
 
-  useEffect((): void => {
-    setTable(createTable)
+  useEffect((): void => {   // эфект срабатывает  если меняется значение одного из массива кардинат
+    setTable(createTable);
   }, [letterCoordinates, digitCoordinates]);
   
-  if (height === 0 && width === 0) {
+  if (height === 0 && width === 0) {  
     return (<div>{'задате данные для таблицы'}</div>)
   }
 
-  const handleChange = (option: { iLetter: any; keyNumber: any; text?: string; state?: string; }) => (e: { target: { value: string; }; }) => {
-
+  const handleChange = (option: { iLetter: string; keyNumber: number; text?: string; state?: string; }) => (e: { target: { value: string; }; }) => { // это  событие  перезаписывает данные в ячейку
     const {iLetter, keyNumber} = option;
-
     const newTable = table.map((el) => {
       if (el.lineNumber === keyNumber) {
        el.cells.map((cell) => {
@@ -63,11 +61,10 @@ const Table: React.FC<any> = (props) => {
       }
       return el
     })
-    
     setTable(newTable);
   };
 
-  const handleLooc = (option: { iLetter: any; keyNumber: any; text?: string; state?: string; }) => (e: React.MouseEvent) => {
+  const handleLooc = (option: { iLetter: string; keyNumber: number; text?: string; state?: string; }) => (e: React.MouseEvent) => {  // событие двойного клика которая блокирует ячейку для изменения 
     const {iLetter, keyNumber} = option;
     const newTable = table.map((el) => {
       if (el.lineNumber === keyNumber) {
@@ -83,17 +80,17 @@ const Table: React.FC<any> = (props) => {
     setTable(newTable);
   };
 
-  const handlerAddLetterCoordinates = () => {
-    const newLetters = arrEN.slice(0, letterCoordinates.length + 1)
-    return setLetterCoordinates(newLetters)
+  const handlerAddLetterCoordinates = () => {  // событие добавляет новую букву  в систему кардинат [A, B, C] => [A, B, C, D]
+    const newLetters = arrEN.slice(0, letterCoordinates.length + 1);
+    return setLetterCoordinates(newLetters);
   };
   
-  const handlerAddDigitCoordinates  = () => {
-    const newArr = [...digitCoordinates, digitCoordinates[digitCoordinates.length - 1] + 1]
-    setDigitCoordinates(newArr)
+  const handlerAddDigitCoordinates  = () => { // событие добавляет новую число в систему кардинат [1, 2, 3] => [1, 2, 3, 4]
+    const newArr = [...digitCoordinates, digitCoordinates[digitCoordinates.length - 1] + 1];
+    setDigitCoordinates(newArr);
   };
  
-  const iterCell = () => {
+  const iterCell = () => {   // отрисовка таблицы
     const collorCell = (status: string) => status === 'open' ? {border: 'solid #69c'} : {border: 'solid red'}
     return (
       table.map((el) => {
@@ -120,28 +117,29 @@ const Table: React.FC<any> = (props) => {
 const App: React.FC = () => {
   
   
-  const [size, setSize] = useState({ width: 0, height: 0 });
+  const [size, setSize] = useState({ width: 0, height: 0 }); //  создание наччальных значений хука с созданием размеров таблицы
 
   const { width, height } = size;
 
-    const form =  <div className="App">
+  // создание компонента  отвечающего за создание данных для  создание таблицы
+
+    const form =  (<div className="App">    
         <p>
           <label>Ширина таблицы </label>
-          <input onChange={(e) => setSize({height, width: Number(e.target.value)})} name="width" type="number"  value={width}></input>
+          <input onChange={(e) => setSize({height, width: Number(e.target.value)})} name="width" type="number"  value={width}></input> 
         </p>
         <p>
           <label>Высота таблицы </label>
           <input onChange={(e) => setSize({width, height: Number(e.target.value)})} name="height" type="number"  value={height}></input>
         </p> 
-      </div>
+      </div>);
 
     return (
       <div>
         {form}
         {<Table option={size}></Table>}
       </div>
-    )
+    );
   };
-
 
 export default App;
